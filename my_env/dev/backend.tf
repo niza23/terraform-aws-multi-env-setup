bf1254ff0114b8aec611496c57ff9bc082a9c6cd
Nidhi Zala <nidhizala2307@gmail.com>
2025-08-18 19:31:44 +0530

    added backend.tf

---
+++ my_env/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket" # Change this
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}

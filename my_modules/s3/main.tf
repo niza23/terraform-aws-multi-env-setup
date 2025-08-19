9ca57decdf771f06dede8e8d38f5b52844d027ef
Nidhi Zala <nidhizala2307@gmail.com>
2025-08-19 18:18:03 +0530

    Update main.tf

--- my_modules/s3/main.tf
+++ my_modules/s3/main.tf
@@ -1,1 +1,24 @@
+# Create the S3 bucket
+resource "aws_s3_bucket" "this" {
+    bucket = var.bucket_name  # Name of the bucket
+    acl    = var.acl          # Access control list (default = private)
+
+# Enable or disable versioning for the bucket
+versioning {
+    enabled = var.versioning
+}
+
+# Apply user-defined tags
+    tags = var.tags
+}
+
+# Block all public access to the bucket for security
+resource "aws_s3_bucket_public_access_block" "this" {
+    bucket = aws_s3_bucket.this.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
 
